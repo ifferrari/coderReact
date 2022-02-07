700319892ca5c760529c073e0682c95db4184ba2
Ignacio Ferrari <ignaciofferrari@gmail.com>
2022-02-07 19:10:23 -0300

    NavBar Components

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+
+const NavBar = () => {
+    return(
+        <nav class ="menuContainer">
+            <div class="menuIndex">
+                <div><a>Inicio</a></div>
+                <div><a>Computadoras</a></div>
+                <div><a>Tablets</a></div>
+                <div><a>Contacto</a></div>
+            </div>
+        </nav>
+    )
+}
+
+
+export default NavBar;
